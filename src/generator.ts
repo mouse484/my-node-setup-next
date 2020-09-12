fa6c95c4fe5ae31c26b3ecc50e5a26f55d2387f6
mouse_484 <escmouse484@gmail.com>
2020-09-12 07:13:43 +0000

    add package

--- src/generator.ts
+++ src/generator.ts
@@ -2,5 +2,5 @@ import { PromptAnsers } from "./prompt";
 import { Package } from "./generators/package";
 
 export const generator = ({ name, description, author }: PromptAnsers) => {
-  new Package().make({ name, description, author });
+  new Package().add("typescript", true).make({ name, description, author });
 };
--- src/generators/package.ts
+++ src/generators/package.ts
@@ -1,14 +1,32 @@
 import { Generator } from ".";
 import { PromptAnsers } from "../prompt";
 import { makeFile, formatJson } from "../lib";
+import { dependencies as dependenciesList } from "../lib/package.json";
 
 export type packageJson = Pick<PromptAnsers, "name" | "description" | "author">;
 
+export type dependenciesType = keyof typeof dependenciesList;
+
 export class Package extends Generator {
+  private dependencies: { [key: string]: string } = {};
+  private devDependencies: { [key: string]: string } = {};
   constructor() {
     super("package.json");
   }
+  add(name: dependenciesType, dev?: true): this {
+    if (dev) {
+      this.devDependencies[name] = dependenciesList[name];
+    } else {
+      this.dependencies[name] = dependenciesList[name];
+    }
+    return this;
+  }
   make(data: packageJson) {
-    makeFile(this.name, formatJson(data));
+    const packageData = {
+      ...data,
+      dependencies: this.dependencies,
+      devDependencies: this.devDependencies,
+    };
+    makeFile(this.name, formatJson(packageData));
   }
 }
