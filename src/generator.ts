1fc4701cc6f19eec3e9dcde4e41b57948fc67298
mouse_484 <escmouse484@gmail.com>
2020-09-14 08:48:36 +0000

    if typescript

--- src/generator.ts
+++ src/generator.ts
@@ -1,6 +1,17 @@
 import { PromptAnsers } from "./prompt";
 import { Package } from "./generators/package";
 
-export const generator = ({ name, description, author }: PromptAnsers) => {
-  new Package().add("typescript", true).make({ name, description, author });
+export const generator = ({
+  name,
+  description,
+  author,
+  typescript,
+}: PromptAnsers) => {
+  const packageGenerator = new Package();
+
+  if (typescript) {
+    packageGenerator.add("typescript", true);
+  }
+
+  packageGenerator.make({ name, description, author });
 };
