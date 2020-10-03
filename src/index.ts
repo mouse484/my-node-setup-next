28d39d8f64013783bc742770d2bb3cb45e90d107
mouse_484 <escmouse484@gmail.com>
2020-10-03 04:05:23 +0000

    make cli
    Fixes mouse484/my-node-setup-next#19

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+#!/usr/bin/env ts-node-script
+
 import { prompt } from './prompt';
 import { generator } from './generator';
 
 const main = async () => {
   const promptAnswers = await prompt();
-  
+
   generator(promptAnswers);
 };
 
