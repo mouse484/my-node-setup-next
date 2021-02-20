acff2a78d53a316a7fd1993c91a4e3a52a1fbd58
mouse_484 <escmouse484@gmail.com>
2021-02-20 10:03:08 +0000

    set Generator

--- src/generators/Base.ts
+++
@@ -1,9 +0,0 @@
-import { makeFile } from '../lib';
-
-export abstract class Generator {
-  constructor(public name: string) {}
-  abstract make(data: unknown): void;
-  protected makeFile(data: string): void {
-    makeFile(this.name, data);
-  }
-}
