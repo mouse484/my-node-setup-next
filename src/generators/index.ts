75ee5a9e1c1c199c7b6ae077d3d168f55b9451cd
mouse_484 <escmouse484@gmail.com>
2021-02-20 10:07:30 +0000

    [Generator] add prettierrc
    Fixes mouse484/my-node-setup-next#31

--- src/generators/index.ts
+++ src/generators/index.ts
@@ -1,11 +1,11 @@
 import { makeFile } from '../lib';
 
 export abstract class Generator {
-    constructor(public name: string) { }
-    abstract make(data: unknown): void;
-    protected makeFile(data: string): void {
-        makeFile(this.name, data);
-    }
+  constructor(public name: string) {}
+  abstract make(data: unknown): void;
+  protected makeFile(data: string): void {
+    makeFile(this.name, data);
+  }
 }
 
 export { formatJson } from '../lib';
@@ -13,4 +13,5 @@ export { formatJson } from '../lib';
 export * from './package';
 export * from './gitignore';
 export * from './eslintrc';
+export * from './prettierrc';
 export * from './tsconfig';
